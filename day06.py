4f04cc4b985a7541757f5b0ae9ac167ed030517e
Guido Diepen <site-github@guidodiepen.nl>
2020-12-19 14:27:22 +0100

    Add part2 of day6

--- day06.py
+++ day06.py
@@ -1,6 +1,7 @@
 import os
 import itertools
 import re
+import collections
 
 
 def part1(input_file):
@@ -18,6 +19,28 @@ def part1(input_file):
 
 
 
+def part2(input_file):
+    with open(input_file) as f:
+
+        groups_input = f.read().split("\n\n")
+    
+    results = []
+    for group in groups_input:
+
+
+        c = collections.Counter(list(group.strip().replace("\n","")))
+        num_people = len(group.strip().split("\n"))
+
+        all_yes = [letter for letter,count in c.most_common() if count == num_people]
+
+
+        results.append(len(all_yes))
+
+    print(sum(results))
+
+
+
+
     
 
 
@@ -25,6 +48,7 @@ def part1(input_file):
 
 if __name__ == "__main__":
     part1("inputs/day_06.txt")
+    #part2("foo.txt")
     #part1("foo.txt")
-    #part2("inputs/day_06.txt")
+    part2("inputs/day_06.txt")
 
